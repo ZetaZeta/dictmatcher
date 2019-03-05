fa8fc1161e627b28dff69e117a2d68a2092f82e7
Matthew Carson <MatthewSCarson@gmail.com>
2019-03-05 00:10:15 -0500

    Fix views.py so it handles a 'none' result properly.  Tweaking some variable names and add a few more comments.

--- matcherapp/views.py
+++ matcherapp/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .util import get_matching_by_button
 
+# Handles a request.  Most of the heavy lifting is in a utility file,
+# which keeps views clean.
 def index(request):
-    outputList = []
-    context = {'outputList': outputList}
-    
+    output_list = []
+    context = {'outputList': output_list}
+
+    # Postback when button is pushed.
+    # When multiple matchings are implemented,
+    # which one is applied will be determined by which button was pushed.
     if request.method == 'POST':
-        outputList.extend(get_matching_by_button(request))
+        matching = get_matching_by_button(request)
+        if matching == None:
+            output_list.append("[None].  No matches found.")
+        else:
+            output_list.extend(matching)
     return render(request, 'dictmatcher/index.html', context)
